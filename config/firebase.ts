bc8a9d0759d7a987151124fe0bfd9774940ae491
Fauzan <heavenzownz@gmail.com>
2026-02-15 17:09:09 +0800

    Added firebase analytics and updated firebases version

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,6 +1,5 @@
 // @/config/firebase.js
 import auth from "@react-native-firebase/auth";
 import firestore from "@react-native-firebase/firestore";
-import storage from "@react-native-firebase/storage";
 
-export { auth, firestore, storage };
+export { auth, firestore };
